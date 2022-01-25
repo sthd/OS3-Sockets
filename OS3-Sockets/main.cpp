9be4a158d153100cf07fd9e7f9b4233a8ab25844
sthd <sch@campus.technion.ac.il>
2022-01-25 12:58:00 +0200

    Initial Commit

---
+++ OS3-Sockets/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OS3-Sockets
+//
+//  Created by Elior Schneider on 25/01/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
