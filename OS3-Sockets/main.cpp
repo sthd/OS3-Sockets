8314311f29676a849b3ef1b0d0eb19bdfce606e1
sthd <sch@campus.technion.ac.il>
2022-01-27 11:20:10 +0200

    post-VM-changes

--- OS3-Sockets/main.cpp
+++ OS3-Sockets/main.cpp
@@ -7,7 +7,8 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <string.h>
+#include <cstring>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -28,7 +29,7 @@ struct wrq{
     uint16_t opcode;    //2 bytes
     string fileName;
     string transMode;
-} __attribute__((packed));
+};
 
 struct ack{
     uint16_t opcode;    //2 bytes
@@ -53,8 +54,10 @@ string octet = "octet";
 
 void checkArguments(int argc, const char * argv[]){
     bool illegal = false;
-    int legalSize = sizeof(unsigned short);
-    if ( argc!=3 || (sizeof(argv[1]) > legalSize ) || (sizeof(argv[2]) > legalSize ) || (sizeof(argv[3]) > legalSize ) ){
+   // int legalSize = sizeof(unsigned short);
+// || ((int)sizeof(argv[1]) > legalSize ) || ((int)sizeof(argv[2]) > legalSize ) || ((int)sizeof(argv[3]) > legalSize )
+    if ( argc!=4){
+    cout << argc << endl;
         cout << "TTFTP_ERROR: illegal argument" << endl;
         exit(1);
     }
@@ -139,55 +142,65 @@ int main(int argc, const char * argv[]) {
             if (recvMsgSize == 0)
                 continue;
             else{
-                perror("TTFTP_ERROR:"); // ? should we specifr errors? or let perror do it
+                perror("TTFTP_ERROR: 1"); // ? should we specifr errors? or let perror do it
                 exit(1);
             }
         }
         
         int opcode = ntohs(*(uint16_t*) &echoBuffer);
         if (opcode != WRQ_OPCODE){
-            cerr << "TTFTP_ERROR:" << endl; // ? should we specifr errors? or let perror do it
+            cerr << "TTFTP_ERROR: 2        " << endl; // ? should we specifr errors? or let perror do it
             continue;
         }
         string fileName = &echoBuffer[2];
         if (fileName.size() == 0){ //?
             // MARK - not given a fileName
-            cerr << "TTFTP_ERROR:" << endl; // ? should we specifr errors? or let perror do it
+            cerr << "TTFTP_ERROR: 3" << endl; // ? should we specifr errors? or let perror do it
             continue;
         }
         
         if (fileName[0] != '/'){
             //
-            cerr << "TTFTP_ERROR:" << endl; // ? should we specifr errors? or let perror do it
+            cerr << "TTFTP_ERROR: 4" << endl; // ? should we specifr errors? or let perror do it
 
         }
         
         string mode = &echoBuffer[2 + fileName.length() + 1];
         if (mode != octet){
-            cerr << "TTFTP_ERROR:" << endl; // ? should we specifr errors? or let perror do it
+            cerr << "TTFTP_ERROR: 5" << endl; // ? should we specifr errors? or let perror do it
             continue;
         }
-        char* filePath;
-        strcpy(filePath, fileName.c_str() );
+        //char* filePath=NULL;
+    //filePath = new char[fileName.length() + 1];
+        //strcpy(filePath, fileName.c_str() );
+    cout << "I just strcpy" << endl;
+    //filePath[fileName.length()]='\0';
         //const char* filePath= "/Users/er/Mcode/OS3-Sockets/OS3-Sockets/a.txt"; //string.c_str()
-        fdFile=open(filePath, O_RDONLY);
-        
+        fdFile=open(fileName.c_str(), O_RDONLY);
+  
         if (fdFile == -1){ // file doesn't exist :-)
-            fdFile = open(filePath, O_CREAT|O_WRONLY);
+            fdFile = open(fileName.c_str(), O_CREAT|O_WRONLY);
             if (fdFile == -1){
+         cerr << "TTFTP_ERROR: 6" << endl;
                 //cannot create file
                 exit(1);
             }
         }
         else{
             // already exists!!
-            exit(1);
+        cerr << "TTFTP_ERROR: 7" << endl;
+        if (close(fdFile) == -1){
+                    //ERROR CANT CLOSE FILE
+             cerr << "TTFTP_ERROR: 5B" << endl;
+                }
+              exit(1);
         }
 
         if (sendAck(receivedBlock, serverSocket, echoClntAddr, clntSockSize) != true){
             // ERROR CANT SEND ACK
             if (close(fdFile) == -1){
                 //ERROR CANT CLOSE FILE
+         cerr << "TTFTP_ERROR: 8" << endl;
             }
             exit(1);
         }
@@ -210,6 +223,7 @@ int main(int argc, const char * argv[]) {
                     perror(" mmmmm "); //TFTP FAIL SELECT
                     if (close(fdFile) == -1){
                         //ERROR CANT CLOSE FILE
+             cerr << "TTFTP_ERROR: 9" << endl;
                     }
                     exit(1);
                 }
@@ -218,14 +232,17 @@ int main(int argc, const char * argv[]) {
                     timeoutExpiredCount++;
                     if (sendAck(receivedBlock, serverSocket, echoClntAddr, clntSockSize) != true){
                         // ERROR CANT SEND ACK
+             cerr << "TTFTP_ERROR: 10" << endl;
                         if (close(fdFile) == -1){
                             //ERROR CANT CLOSE FILE
+                 cerr << "TTFTP_ERROR: 11" << endl;
                         }
                         exit(1);
                     }
                     if (timeoutExpiredCount > allowedFailures){
                         cerr << "Abandoning file transmission" << endl;
                         // @@@
+
                     }
                 }
                 
@@ -238,6 +255,7 @@ int main(int argc, const char * argv[]) {
                 perror("");
                 if (close(fdFile) == -1){
                     //ERROR CANT CLOSE FILE
+             cerr << "TTFTP_ERROR: 12" << endl;
                 }
                 exit(1);
             }
@@ -258,6 +276,7 @@ int main(int argc, const char * argv[]) {
                 // ERROR CANT SEND ACK
                 if (close(fdFile) == -1){
                     //ERROR CANT CLOSE FILE
+             cerr << "TTFTP_ERROR: 13" << endl;
                 }
                 exit(1);
             }
@@ -267,6 +286,7 @@ int main(int argc, const char * argv[]) {
                 writtenBytes = write(fdFile, &echoBuffer[HEADER], dataNeto);
                 if (writtenBytes != dataNeto){
                     // WRITE FALIED
+             cerr << "TTFTP_ERROR: 14" << endl;
                     exit(1);
                 }
             }
@@ -276,9 +296,10 @@ int main(int argc, const char * argv[]) {
         
         if (close(fdFile) == -1){
             //ERROR CANT CLOSE FILE
+         cerr << "TTFTP_ERROR: 15" << endl;
             exit(1);
         }
-        
+        //delete [] filePath;
     }//while(1)
     
     return 0;
